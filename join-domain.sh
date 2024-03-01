0f0981c1d1a47d9dc14197cd4217379112a1e760
5opk4 <161751638+5opk4@users.noreply.github.com>
2024-03-01 11:38:26 +0700

    Update join-domain.sh

--- join-domain.sh
+++ join-domain.sh
@@ -11,15 +11,14 @@ echo "Запуск..."
 
 #Запрос данных для входа в домен.
 read -p "Введите имя доменного пользователя с правами администратора: " adm
-read -p "Введите имя домена верхнего уровня (com, ru, work и др): " tld
-read -p "Введите имя домена: " dmn
+read -p "Введите полное имя домена: " dmn
 host=$(hostnamectl hostname)
 
 #Установка необходимых пакетов.
 apt update
 apt install -y realmd krb5-user
 
-pack=$(realm discover $dmn.$tld | grep 'package' | awk '{print $2}')
+pack=$(realm discover $dmn | grep 'package' | awk '{print $2}')
 
 if [[ -n $pack ]]; then
 		apt install -y $pack
@@ -27,7 +26,7 @@ fi
 
 #Вход в домен.
 echo -e "Необходимые пакеты \e[1;32mуспешно\e[0m установлены.\nВведите пароль для входа в домен - \e[1;33m$dmn.$tld\e[0m"
-join=$(realm join --verbose $dmn.$tld --user=$adm --user-principal="host/$host@$dmn.$tld"--computer-ou="OU=Linux Servers, OU=KOM, DC=$dmn, DC=$tld" --install=/)
+join=$(realm join --verbose $dmn --user=$adm --user-principal="host/$host@$dmn" --install=/)
 
 if [[ $? -eq 0 ]]; then
 		echo -e "\e[1;32mЭтот компьютер подключился к домену.\e[0m"
